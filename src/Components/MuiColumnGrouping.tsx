7a34f83314a82ac4f90f43f4ede28def2b38a9ee
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-06 16:13:08 +0530

    column Grouping Example

---
+++ src/Components/MuiColumnGrouping.tsx
@@ -0,0 +1,51 @@
+import { DataGrid,GridColDef } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+
+const rows = [
+  { id: 1, firstName: "Jon", lastName: "Snow", age: 14 },
+  { id: 2, firstName: "Cersei", lastName: "Lannister", age: 31 },
+  { id: 3, firstName: "Jaime", lastName: "Lannister", age: 31 },
+  { id: 4, firstName: "Arya", lastName: "Stark", age: 11 },
+];
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+
+  { field: "firstName", headerName: "First Name", width: 150 },
+
+  { field: "lastName", headerName: "Last Name", width: 150 },
+
+  { field: "age", headerName: "Age", width: 110, type: "number" },
+];
+
+const columnGroupingModel = [
+  {
+    groupId: "internal",
+    headerName: "Internal Data",
+    children: [{ field: "id" }],
+  },
+  {
+    groupId: "nameGroup",
+    headerName: "Full Name",
+    children: [
+      { field: "firstName" },
+      { field: "lastName" },
+    ],
+  },
+  {
+    groupId: "details",
+    headerName: "Details",
+    children: [{ field: "age" }],
+  },
+];
+function MuiColumnGrouping() {
+  return (
+    <Box sx={{height:400, width:'100%'}}>
+        <DataGrid rows={rows} columns={columns} 
+        columnGroupingModel={columnGroupingModel}
+        columnGroupHeaderHeight={40}/>
+      
+    </Box>
+  );
+}
+export default MuiColumnGrouping;
