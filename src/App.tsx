deb78a60cd8ae74c39c63b0d6ef57ac8ec373c42
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-07 13:14:27 +0530

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import "./App.css";
-import { useState } from "react";
-import MuiFormDialog from "./Components/MuiFormDialog";
-
+import './App.css';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import MuiPicker from './Components/MuiPicker';
 function App() {
-
-  const [userData, setUserData] = useState<any>(null);
-
-  const handleFormSubmit = (data: any) => {
-    setUserData(data);
-  };
-
-  return (
+   return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
     <div className="App">
-
-      <MuiFormDialog onSubmit={handleFormSubmit} />
-
-      {userData && (
-        <div>
-          <p>Username: {userData.username}</p>
-          <p>Password: {userData.password}</p>
-          <p>Email: {userData.email}</p>
-          <p>Phone: {userData.phone}</p>
-          
-        </div>
-      )}
-
+      <MuiPicker/>
     </div>
+    </LocalizationProvider>
   );
 }
 
