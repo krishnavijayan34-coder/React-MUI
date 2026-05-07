4e3097a0330bcd088ee589e07f5328d1af8cc785
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-07 12:01:20 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import './App.css';
-import MUITypography from './Components/MUITypography';
-import MuiButtons from './Components/MuiButtons';
+import "./App.css";
+import { useState } from "react";
+import MuiFormDialog from "./Components/MuiFormDialog";
 
 function App() {
+
+  const [userData, setUserData] = useState<any>(null);
+
+  const handleFormSubmit = (data: any) => {
+    setUserData(data);
+  };
+
   return (
     <div className="App">
-    <MUITypography/>
-    <MuiButtons/>
-      
+
+      <MuiFormDialog onSubmit={handleFormSubmit} />
+
+      {userData && (
+        <div>
+          <p>Username: {userData.username}</p>
+          <p>Password: {userData.password}</p>
+          <p>Email: {userData.email}</p>
+          <p>Phone: {userData.phone}</p>
+          
+        </div>
+      )}
+
     </div>
   );
 }
